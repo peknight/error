310604c46e1821c7659872c0adf602757fd945cc
peknight <JKpeknight@gmail.com>
2024-11-15 16:18:32 +0800

    update

--- error-core/shared/src/main/scala/com/peknight/error/Error.scala
+++ error-core/shared/src/main/scala/com/peknight/error/Error.scala
@@ -138,7 +138,7 @@ object Error extends Error with ErrorInstances:
     base(e) match
       case err: Lift[?] => err.error match
         case m: String => m
-        case t: Throwable => t.getMessage
-        case error => s"${errorType(err)}:$err"
+        case t: Throwable => s"${errorType(t)}:${t.getMessage}"
+        case error => s"${errorType(error)}:$error"
       case err => errorType(err)
 end Error
